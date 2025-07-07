dd9bdeb10e87c89836d6a4e9075a606ea803562d
Ivriel Gunawan <gunawanivriel@gmail.com>
2025-07-07 16:53:45 +0700

    Updating menu

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 @Component({
   selector: 'app-menu',
-  imports: [MatMenuModule,MatButtonModule],
+  imports: [MatMenuModule,MatButtonModule,MatIconModule],
   templateUrl: './menu.component.html',
-  styleUrl: './menu.component.css'
+  styleUrl: './menu.component.scss'
 })
 export class MenuComponent {
 
